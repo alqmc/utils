3d73b8d286b69619d1a3dfa2c654e49584c55d30
alqmc <Yonghu520@outlook.com>
2022-07-07 10:25:15 +0800

    fix: detail

--- package/object.ts
+++ package/object.ts
@@ -1,30 +1,54 @@
-export const isObject = (val: any) => val && typeof val === 'object';
+type dataType =
+  | 'boolean'
+  | 'number'
+  | 'string'
+  | 'function'
+  | 'array'
+  | 'date'
+  | 'regExp'
+  | 'object'
+  | 'null'
+  | 'undefined';
+export const typeOf = (obj: any): dataType => {
+  const { toString } = Object.prototype;
+  const map: { [key: string]: dataType } = {
+    '[object Boolean]': 'boolean',
+    '[object Number]': 'number',
+    '[object String]': 'string',
+    '[object Function]': 'function',
+    '[object Array]': 'array',
+    '[object Date]': 'date',
+    '[object RegExp]': 'regExp',
+    '[object Undefined]': 'undefined',
+    '[object Null]': 'null',
+    '[object Object]': 'object',
+  };
+  return map[toString.call(obj)];
+};
+
+export const isObject = (val: any) => val && typeOf(val) === 'object';
+export const isArray = (val: any) => val && Array.isArray(val);
+export const isReferenceType = (val: any) => isObject(val) || isArray(val);
+
 export const mergeArrayWithDedupe = (a: any[], b: any[]) =>
   Array.from(new Set([...a, ...b]));
 
-/**
- *
- * @param target
- * @param source
- * @returns
- */
 export const deepMerge = (
   target: Record<string, any>,
   source: Record<string, any>
 ) => {
-  for (const key of Object.keys(source)) {
-    const oldVal = target[key];
-    const newVal = source[key];
-
-    if (Array.isArray(oldVal) && Array.isArray(newVal)) {
-      target[key] = mergeArrayWithDedupe(oldVal, newVal);
-    } else if (isObject(oldVal) && isObject(newVal)) {
-      target[key] = deepMerge(oldVal, newVal);
-    } else {
-      target[key] = newVal;
+  if (isObject(target) && isObject(source)) {
+    for (const key in source) {
+      target[key] =
+        target[key] && isReferenceType(target[key])
+          ? deepMerge(target[key], source[key])
+          : source[key];
     }
+  } else if (Array.isArray(target) && Array.isArray(source)) {
+    target = mergeArrayWithDedupe(target, source);
+  } else {
+    target = source;
   }
-
   return target;
 };
 
--- test/object.test.ts
+++ test/object.test.ts
@@ -1,29 +1,51 @@
 import { expect, test } from 'vitest';
-import { deepMerge } from '../package/object';
+import { deepMerge, isObject, mergeArrayWithDedupe } from '../package/object';
 
 const testObj1 = {
   a: 1,
-  b: [1, 2, 3, 4],
+  b: [1, 2, 3, 4, { a: 1, s: [32] }],
   c: {
     start: { a: 1 },
   },
 };
 const testObj2 = {
-  b: [1, 5, 6, 8, 7, 4],
+  b: [1, 5, 6, 8, 7, 4, { a: 1, s: [11] }],
   c: { start: [1, 2] },
 };
 const result = {
   a: 1,
-  b: [1, 2, 3, 4, 5, 6, 8, 7],
-  c: {
-    start: {
-      '0': 1,
-      '1': 2,
+  b: [
+    1,
+    2,
+    3,
+    4,
+    {
       a: 1,
+      s: [32],
     },
+    5,
+    6,
+    8,
+    7,
+    {
+      a: 1,
+      s: [11],
+    },
+  ],
+  c: {
+    start: [1, 2],
   },
 };
 
 test('deepMerge', () => {
   expect(deepMerge(testObj1, testObj2)).toEqual(result);
+});
+
+test('isObject', () => {
+  expect(isObject(testObj1)).toBeTruthy();
+  expect(isObject(1)).toBeFalsy();
+});
+
+test('mergeArrayWithDedupe', () => {
+  expect(mergeArrayWithDedupe([1, 1], [2, 2])).toEqual([1, 2]);
 });
